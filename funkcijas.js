a939575205b2861952891fd81ed409a64f945d76
SussyJustine69 <113004046+SussyJustine69@users.noreply.github.com>
2023-01-11 09:42:22 +0200

    qq

--- funkcijas.js
+++ funkcijas.js
@@ -1,2 +1,38 @@
+//Jānolasa ievadītais
+//Jaizvēlās skaitlis (datoram)
+//Jāpārbauda ievadītais
+//Jāizvada atbilde
 
- function nolasitievadito();{console.log}
+let datoraSkaitlis = 10;
+
+function izvaditAtbildi(atbilde){
+document.getElementById("atbilde").innerHTML=atbilde;
+
+}
+
+ function nolasitIevadito(){
+let ievaditais = document.getElementById("apple").value;
+console.log(ievaditais);
+return ievaditais;
+
+ }
+function salidzinatSkaitlus(cilvekaSkaitlis){
+if (isNaN(cilvekaSkaitlis)){return "Ievadi skaitli mūllāp"};
+if (cilvekaSkaitlis>datoraSkaitlis){ return "datora skaitlis ir mazāks, ka tavs pincits"};
+if(cilvekaSkaitlis<datoraSkaitlis){ return "datora skaitlis ir lielāks idiņ"};
+return "OPAAA TEV NAV MAZS PINCITS";
+}
+
+
+ function veiktParbaudi(){
+console.log("Pārbaudīts!", datoraSkaitlis);
+let minejums = nolasitIevadito();
+let rezultats = salidzinatSkaitlus(minejums);
+izvaditAtbildi(rezultats);
+ }
+
+ function nomainitskaitli(){
+datoraSkaitlis = Math.round(Math.random()*100);
+console.log(datoraSkaitlis);
+izvaditAtbildi("Jauna Spēle");
+ }
